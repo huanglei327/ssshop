a27e69458da6d49e549a92ec5ed11cedb494460f
yangjun <yangjun>
2019-06-04 18:02:12 +0800

    2019-06-04.1

--- src/components/commodityItem.vue
+++ src/components/commodityItem.vue
@@ -1,13 +1,14 @@
 <template>
   <div>
-    <div class="c-main">
+    <div class="c-main"
+         style="display:block;">
       <div class="c-content"
            v-for="(item,index) in items"
            :key="index">
-
         <div class="c-single"
              @click="GoDetails(item)">
           <div class="c-img">
+            <div class="c-orignTitle">{{item.goodsBrief}}</div>
             <img :src="item.listPicUrl"
                  mode="scaleToFill"
                  lazy-load="true"
@@ -37,9 +38,26 @@ export default {
       .c-img {
         background: #f5f5f5;
         width: 100%;
+        position: relative;
+        .c-orignTitle {
+          position: absolute;
+          bottom: 0;
+          left: 0;
+          width: 100%;
+          height: 25px;
+          line-height: 25px;
+          background: @bgantiquewhite;
+          overflow: hidden;
+          text-overflow: ellipsis;
+          white-space: nowrap;
+          font-size: 12px;
+          color: @colorantiquewhite;
+          letter-spacing: 1px;
+        }
         .itemimg {
           width: 100%;
-          height: 150px;
+          max-height: 150px;
+          object-fit: cover;
         }
       }
       .c-name {
